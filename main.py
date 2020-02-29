85dbb02a76b21c2db6382dfacb4c08addb12960e
mahbu006 <50161218+mahbu006@users.noreply.github.com>
2020-02-28 22:45:14 -0600

    main.py completed

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # Encephalo Investments Coding Pre-Test - main.py
 import pandas as pd
 
-
 def main():
     # main function.
     # Return the top 10 companies with the highest market cap.
-    data = pd.read_csv("./data.csv") # read csv using pandas
+    data = pd.read_csv("./data.csv") # read csv using pandas 
+    
+    # Found Market Cap column, replaced '$' with empty space, and dropped 'N/A' indicies
+    # replaced 'M' 'B' with appropriate values and represented it as a number, and found 
+    # the 10 largest market caps
+    # NOTE: unable to find the corresponding companies :(
 
+    market_cap = data['market_cap'].str.replace(r'$', '').dropna()
+    print(market_cap.replace({'M': '*1e6', 'B': '*1e9'}, regex=True).map(pd.eval).nlargest(10))
 
 if __name__ == "__main__":
     main()
